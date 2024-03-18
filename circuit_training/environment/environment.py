fdbbe8ae9a42c9cf47122a1d4d0c678dbbdf5d7f
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-03-18 22:02:13 +0000

    Gave circuitenv a seed to be compatible with gymnasium

--- circuit_training/environment/environment.py
+++ circuit_training/environment/environment.py
@@ -201,6 +201,7 @@ class CircuitEnv(gym.Env):
     super(CircuitEnv, self).__init__()
 
     self._global_seed = global_seed
+    self.seed = global_seed # for compatibility with gymnasium
     if not netlist_file:
       raise ValueError('netlist_file must be provided.')
 
@@ -542,7 +543,7 @@ class CircuitEnv(gym.Env):
     self._current_mask = self._get_mask()
     self._observation_extractor.reset()
     self._episode_start_time = time.time()
-
+    self.seed = seed # for compatibility with gymnasium
     if self._use_legacy_reset:
       return self._get_obs()
     else:
