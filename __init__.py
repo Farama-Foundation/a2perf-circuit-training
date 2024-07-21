06a7dfcad8df35949732b24d0bf5cd83542f4cb2
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-07-20 21:48:50 -0400

    Separated different netlists into different envs

--- __init__.py
+++ __init__.py
@@ -1,51 +1,39 @@
 import os
-
-import gym as legacy_gym
 import gymnasium as gym
 import pkg_resources
 
-netlist_file_path = pkg_resources.resource_filename(
-    "a2perf",
-    "domains/circuit_training/circuit_training/environment/test_data/ariane/netlist.pb.txt",
-)
 
-init_placement_file_path = pkg_resources.resource_filename(
-    "a2perf",
-    "domains/circuit_training/circuit_training/environment/test_data/ariane/initial.plc",
-)
+def register_circuit_env(id_suffix, netlist_name):
+    netlist_file_path = pkg_resources.resource_filename(
+        "a2perf",
+        f"domains/circuit_training/circuit_training/environment/test_data/{netlist_name}/netlist.pb.txt",
+    )
+    init_placement_file_path = pkg_resources.resource_filename(
+        "a2perf",
+        f"domains/circuit_training/circuit_training/environment/test_data/{netlist_name}/initial.plc",
+    )
+    plc_wrapper_main = pkg_resources.resource_filename(
+        "a2perf", "domains/circuit_training/bin/plc_wrapper_main"
+    )
+    # Fallback to global installation path
+    if not os.path.exists(plc_wrapper_main):
+        plc_wrapper_main = "/usr/local/bin/plc_wrapper_main"
 
-plc_wrapper_main = pkg_resources.resource_filename(
-    "a2perf", "domains/circuit_training/bin/plc_wrapper_main"
-)
+    gym.envs.register(
+        id=f"CircuitTraining-{id_suffix}-v0",
+        apply_api_compatibility=False,
+        disable_env_checker=False,
+        entry_point="a2perf.domains.circuit_training.circuit_training.environment.environment:create_circuit_environment",
+        kwargs=dict(
+            netlist_file=netlist_file_path,
+            init_placement=init_placement_file_path,
+            plc_wrapper_main=plc_wrapper_main,
+            netlist_index=0,
+            use_legacy_step=False,
+            use_legacy_reset=False,
+        ),
+    )
 
-# Fallback to global installation path
-if not os.path.exists(plc_wrapper_main):
-    plc_wrapper_main = "/usr/local/bin/plc_wrapper_main"
 
-gym.envs.register(
-    id="CircuitTraining-v0",
-    apply_api_compatibility=False,
-    disable_env_checker=False,
-    entry_point="a2perf.domains.circuit_training.circuit_training.environment.environment:create_circuit_environment",
-    kwargs=dict(
-        netlist_file=netlist_file_path,
-        init_placement=init_placement_file_path,
-        plc_wrapper_main=plc_wrapper_main,
-        netlist_index=0,
-        use_legacy_step=False,
-        use_legacy_reset=False,
-    ),
-)
-
-legacy_gym.envs.register(
-    id="CircuitTraining-v0",
-    entry_point="a2perf.domains.circuit_training.circuit_training.environment.environment:create_circuit_environment",
-    kwargs=dict(
-        use_legacy_step=True,
-        use_legacy_reset=True,
-        netlist_file=netlist_file_path,
-        init_placement=init_placement_file_path,
-        plc_wrapper_main=plc_wrapper_main,
-        netlist_index=0,
-    ),
-)
+register_circuit_env("Ariane", "ariane")
+register_circuit_env("ToyMacro", "toy_macro_stdcell")
